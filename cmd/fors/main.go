b2ab986b578f95b6e0327750e5db90363c2b948e
Leonid Konovalov <simplesession@gmail.com>
2022-06-20 10:44:18 +0300

    added comments

--- cmd/fors/main.go
+++ cmd/fors/main.go
@@ -3,11 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// for {
+	// for {} // while true
 
-	// } // while true
 	for i := 0; i < 10; i++ {
-
+		fmt.Println(i)
 	}
 
 	for i, v := range []int{1, 2, 3} {
